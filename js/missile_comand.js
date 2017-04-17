290ca439de7bf4fb11958b1d2e2f1e7dddc454f0
urzp <urzp002@mail.ru>
2017-04-17 21:50:25 +0300

    create missle_comand & init

--- js/missile_comand.js
+++ js/missile_comand.js
@@ -1,4 +1,30 @@
-var canvas = document.getElementById('canvas'); 
-var c = canvas.getContext('2d'); 
-c.fillStyle = "red"; 
-c.fillRect(100,100,400,300); 
+var missle_command = missle_command || {};
+
+//missle_command.canvas = document.getElementById('canvas'); 
+//missle_command.ctx = canvas.getContext('2d'); 
+//missle_command.FPS = 30;
+
+missle_command.init = function(){
+        this.canvas = document.getElementById('canvas'); 
+        this.ctx = canvas.getContext('2d'); 
+        this.FPS = 30;
+        this.ctx.clearRect(0, 0, 800, 600);
+        this.ctx.fillStyle = "#000"; 
+        this.ctx.fillRect(0,0,800,600); 
+}
+    
+missle_command.Base = {};
+missle_command.Enimy = {};
+
+
+missle_command.start = setInterval(function() {
+  //update();
+  //draw();
+}, 1000/missle_command.FPS);
+
+$(document).ready(function(){
+    missle_command.init();
+    //missle_command.start();
+})
+
+
